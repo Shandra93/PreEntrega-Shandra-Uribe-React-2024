b9a3aaf0abcfda30e63ef557e4174b3913d73742
Shandra <shandra_a7x@hotmail.com>
2024-07-30 18:00:58 -0600

    Cambios

---
+++ src/App.jsx
@@ -0,0 +1,29 @@
+import './App.css'
+import TituloComponente from './components/TituloComponente'
+
+function App() {
+  const misEstilos = {
+    color:'#887766', 
+    fontFamily:'monospace'
+  }
+
+
+  return (
+    <>
+      <h1 style = {misEstilos}> Hola React </h1>
+      {/* Instanciar componente */}
+      <TituloComponente/>
+      <p className='lorem'>
+        Lorem ipsum dolor sit amet consectetur 
+        adipisicing elit. Alias nostrum nesciunt 
+        minus ab dicta pariatur, aspernatur, 
+        quibusdam voluptatum culpa, doloribus 
+        temporibus! Exercitationem quidem 
+        repellendus minima! Perspiciatis 
+        cumque repellat sunt nisi!
+      </p>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/TituloComponente.jsx
@@ -0,0 +1,9 @@
+export default function TituloComponente() {
+    return(
+        <>
+            <section>
+                <h2> Ecommerce </h2>
+            </section>
+        </>
+    )
+}
